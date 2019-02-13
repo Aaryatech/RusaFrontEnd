c9e4d620d111cc7bb9fb7e9ca1671fdef45bc3d3
Shruti Patil <Shruti Patil@192.168.10.220>
2019-02-13 06:27:53 -0500

    ////shruti

--- src/main/java/com/ats/rusafronend/controller/ImageController.java
+++ src/main/java/com/ats/rusafronend/controller/ImageController.java
@@ -1,11 +1,16 @@
 package com.ats.rusafronend.controller;
 
+import java.net.InetAddress;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -14,21 +19,23 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ats.rusafronend.common.Constant;
 import com.ats.rusafronend.model.ImageLink;
 import com.ats.rusafronend.model.NewsDetails;
-
+import com.ats.rusafronend.model.ContactUs;
 
 @Controller
 @Scope("session")
 public class ImageController {
 
 RestTemplate rest = new RestTemplate();
-	
-	@RequestMapping(value = "/NewsDetails/{langId}/{pageId}/{newsblogsId}", method = RequestMethod.GET)
+ContactUs contactUs=new ContactUs();
+ 	@RequestMapping(value = "/NewsDetails/{langId}/{pageId}/{newsblogsId}", method = RequestMethod.GET)
 	public ModelAndView getImageLink(@PathVariable int langId, @PathVariable int pageId,@PathVariable int newsblogsId, HttpServletRequest request, HttpServletResponse response) {
 
 		ModelAndView model = new ModelAndView("content/NewsDetails");
@@ -56,5 +63,71 @@ RestTemplate rest = new RestTemplate();
 		return model;
 	}
 	
+	@RequestMapping(value = "/ContactUs", method = RequestMethod.GET)
+	public ModelAndView ContactUs(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("content/contactUs");
+		try {
 	
+				 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return model;
+	}
+	@RequestMapping(value = "/insertContactUs", method = RequestMethod.POST)
+	public String insertBannerImage(HttpServletRequest request,HttpServletResponse response) {
+
+		 try {
+			 	String name = request.getParameter("name");
+			 	String email = request.getParameter("email");
+			 	String message = request.getParameter("message");
+			 	String mobileNo = request.getParameter("mobileNo");
+			 		
+				Date date = new Date(); // your date
+				SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+				SimpleDateFormat dateTimeInGMT = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+				
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(date);
+				
+				 // editImageLink.setIsActive(1);
+				
+				InetAddress addr = InetAddress.getByName(request.getRemoteAddr());
+		        String hostName = addr.getHostName(); 
+		        String userAgent = request.getHeader("User-Agent"); 
+							contactUs.setIpAddress(hostName);
+							contactUs.setUserAgent(userAgent);
+							contactUs.setAddDate(sf.format(date));
+							contactUs.setContactName(name);
+							contactUs.setEmailId(email);
+							contactUs.setMessage(message);
+							contactUs.setMobileNo(mobileNo);
+							contactUs.setStatus(1);
+							contactUs.setDelStatus(1);
+							
+          	ContactUs res = rest.postForObject(Constant.url + "/saveContactUs", contactUs, ContactUs.class);
+						
+						
+		 }catch (Exception e) {
+			e.printStackTrace();
+		}
+		 
+	 return "redirect:/ContactUs";
+	}
+	
+	@RequestMapping(value = "/readMore", method = RequestMethod.POST)
+	public ModelAndView readMore(HttpServletRequest request,HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("content/readMore");
+		try {
+	
+				 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return model;
+	}
 }
